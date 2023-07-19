d818ea7d853cc07318c70478dffa1e80a8dc5252
Apple Minecraft <appleminecraft007@gmail.com>
2023-07-19 14:29:39 +0500

    Added glViewport to Renderer::Clear

--- LampEngine/Src/Core/Renderer.cpp
+++ LampEngine/Src/Core/Renderer.cpp
@@ -5,6 +5,7 @@ namespace LampEngine {
 		glm::vec3 color = Window::GetCurrentWindowInstance()->getWindowColor();
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(color.r, color.g, color.b, 1.0f);
+		glViewport(0, 0, Window::GetCurrentWindowInstance()->getWindowSize().x, Window::GetCurrentWindowInstance()->getWindowSize().y);
 	}
 	void Renderer::Render(Entity& entity) {
 		TextureModel& model = entity.getModel();
